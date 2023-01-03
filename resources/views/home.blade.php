770c03994321ea658dee0d8ea788291563a3a07d
yuda25 <yuda7246@gmail.com>
2023-01-03 10:39:31 +0700

    if else blade template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,6 +34,14 @@
     <div class="container">
         <h1>Ini halaman home</h1>
         <h2>welcome {{ $name; }}</h2>
+        <h2>Anda {{ $role; }}</h2>
+        @if ($role == 'admin')
+            <h2>welcome admin</h2>
+        @elseif ($role == 'user')
+            <h2>welcome user</h2>
+        @else 
+            <h2>sadarlah... anda bukan siapa"nya.</h2>
+        @endif
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
